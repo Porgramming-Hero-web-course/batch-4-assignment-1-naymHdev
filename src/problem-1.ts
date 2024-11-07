21b48cb7df0c919c627a2148cf31bf8021e5da08
MacBook Pro <naym100m@gmail.com>
2024-11-07 15:40:54 +0600

    initial setup [Compleate]

---
+++ src/problem-1.ts
@@ -0,0 +1,15 @@
+{
+  // Problem -1
+  /* 
+  Write a TypeScript function sumArray that takes an array of numbers and returns the sum of all elements in the array.
+// Sample Input:
+sumArray([1, 2, 3, 4, 5]);
+
+// Sample Output:
+15;
+*/
+
+
+
+  //
+}
