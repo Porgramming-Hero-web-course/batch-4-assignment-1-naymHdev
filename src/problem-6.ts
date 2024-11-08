978f5729ec1e62bf45836b9ac3308f75eee019a4
MacBook Pro <naym100m@gmail.com>
2024-11-08 09:08:33 +0600

    [solved] problem 6

--- src/problem-6.ts
+++ src/problem-6.ts
@@ -1,27 +1,21 @@
 {
   // Problem 6:
-  /* Define an interface Profile with properties name, age, and email. Create a function updateProfile that accepts an object of type Profile and an object of type Partial representing the updates. The function should return the updated profile.
 
-// Sample Input :
-const myProfile = { name: "Alice", age: 25, email: "alice@example.com" };
-console.log(updateProfile(myProfile, { age: 26 }));
-
-// Sample Output:
-{
-  name: "Alice",
-  age: 26,
-  email: "alice@example.com"
-} */
-
-  interface Person {
+  interface Profile {
     name: string;
     age: number;
     email: string;
   }
 
-  const updateProfile = () => {
-    
-  }
+  const updateProfile = (
+    object: Profile,
+    updates: Partial<Profile>
+  ): Profile => {
+    return { ...object, ...updates };
+  };
+
+  const myProfile = { name: "Alice", age: 25, email: "alice@example.com" };
+  // console.log(updateProfile(myProfile, { age: 26 }));
 
   //
 }
