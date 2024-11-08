acc2e9444891f3c40d8239f1428cb901107b9ea2
MacBook Pro <naym100m@gmail.com>
2024-11-08 09:40:44 +0600

    [solved] 8 number problem finaly solved all problems

--- src/problem-8.ts
+++ src/problem-8.ts
@@ -1,11 +1,15 @@
 {
-  // Problem 8:
-  /* Create a function validateKeys(obj: T, keys: (keyof T)[]) that takes an object obj and an array of keys keys. The function should return true if all of the specified keys exist in the object; otherwise, it should return false.
+  //  Problem 8:
 
-// Sample Input:
-const person = { name: "Alice", age: 25, email: "alice@example.com" };
-console.log(validateKeys(person, ["name", "age"]));
+  const validateKeys = <T extends object>(
+    obj: T,
+    keys: (keyof T)[]
+  ): boolean => {
+    return keys.every((key) => key in obj);
+  };
 
-// Sample Output:
-true; */
+  const person = { name: "Alice", age: 25, email: "alice@example.com" };
+  // console.log(validateKeys(person, ["name", "age"]));
+
+  //
 }
